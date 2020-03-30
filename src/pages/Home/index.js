a234d297be2d23e7ba9e53c97ac5e2f745475281
Jesus Aranda <aranda.jm@icloud.com>
2020-03-30 17:34:18 -0300

    creating home component and using useState hook to handle search text state

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,59 @@
-import React from "react";
-import { Container, Typography } from "@material-ui/core";
+import React, { useState } from "react";
+import {
+  Container,
+  Typography,
+  Card,
+  Grid,
+  TextField,
+  Button
+} from "@material-ui/core";
 
 export default () => {
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearchTextChange = e => {
+    setSearchText(e.target.value);
+  };
+
+  const handleClearTextClick = e => {
+    console.log("handleClearTextClick", e);
+  };
+  const handleSearchTextClick = e => {
+    console.log("handleSearchTextClick", e);
+  };
+
+  console.log(searchText);
   return (
     <Container>
-      <Typography variant="h4">Home</Typography>
+      <Card>
+        {/* This grid is similar to bootstrap col */}
+        <Grid container>
+          <Grid>
+            <Typography>Bienvenido!!</Typography>
+          </Grid>
+          <Grid>
+            <label>Icono</label>
+          </Grid>
+        </Grid>
+        <TextField
+          value={searchText}
+          placeholder="Buscar..."
+          onChange={handleSearchTextChange}
+        />
+        <Grid>
+          <Button variant="contained" onClick={handleClearTextClick}>
+            Limpiar
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            onClick={handleSearchTextClick}
+          >
+            Buscar
+          </Button>
+        </Grid>
+      </Card>
     </Container>
   );
 };
